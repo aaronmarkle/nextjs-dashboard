0efec72f1068e1ff28a73dfa8d002a5506f05b42
Aaron Markle <aaronkmarkle@gmail.com>
2023-11-10 21:41:26 -0800

    updates

---
+++ app/api/auth/route.ts
@@ -0,0 +1,26 @@
+import '@shopify/shopify-api/adapters/node'
+import {shopifyApi, LATEST_API_VERSION} from '@shopify/shopify-api'
+
+const shopify = shopifyApi({
+  // The next 4 values are typically read from environment variables for added security
+  apiKey: 'cda9530df1b3ad8863f567650dc71fdc',
+  apiSecretKey: 'f308cad37fe67183ba4104e3692ded23',
+  scopes: ['read_products'],
+  hostName: 'https://nextjs-dashboard-three-omega.vercel.app/',
+  apiVersion: LATEST_API_VERSION,
+  isEmbeddedApp: true,
+});
+
+import { type NextRequest } from 'next/server'
+ 
+export function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+
+  // The library will return a Response object
+  return shopify.auth.begin({
+    shop: shopify.utils.sanitizeShop(searchParams.get('shop'), true),
+    callbackPath: '/auth/callback',
+    isOnline: false,
+    rawRequest: request,
+  });
+}
